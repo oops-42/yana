3275063fba2b7e534d23c20ab6dc6c0b76f91ea0
plechenko <pavel.lechenko@gmail.com>
2026-07-14 17:00:55 +0200

    feat: add core functionality and help system for YANA.ps1

---
+++ yana.ps1
@@ -0,0 +1,207 @@
+#!/usr/bin/env pwsh
+
+#Requires -Version 5.1
+
+$YANA_TITLE = 'YANA - Yet Another Node Automator'
+$YANA_VERSION = '0.1.0'
+
+function Out-Colored {
+  # .SYNOPSIS
+  # 	Outputs colored text to the output stream.
+  # 	Takes care of logging to a file if $LogFile is specified.
+  # 	If $Quiet is specified, suppresses output.
+  # 	If $NoColor is specified, disables colored output.
+  param(
+    # The color of the text (e.g., 'Red', 'Green', 'Blue').
+    [string]$Color,
+    # The main message to display.
+    [string]$Message,
+    # Additional details to display (optional). Will be displayed in dimmed color.
+    [string]$MessageDetail = ''
+  )
+  if ($Message.Length -gt 0) { $Message = "$Message " }
+  if ($LogFile) {
+    $logMessage = "[$([datetime]::UtcNow.ToString('yyyy-MM-ddTHH:mm:ssZ'))] ${Message}${MessageDetail}"
+    try {
+      Add-Content -Path $LogFile -Value $logMessage -Force -ErrorAction Ignore
+    } catch {
+      Write-Warning "Failed to write to log file '$($LogFile)': $($_.Exception.Message)"
+    }
+  }
+  if ($Quiet) { return }
+  if ($NoColor) {
+    $message = "${Message}$MessageDetail"
+  } else {
+    $colorCode = switch ($Color) {
+      'Black' { 30 }
+      'Red' { 31 }
+      'Green' { 32 }
+      'Yellow' { 33 }
+      'Blue' { 34 }
+      'Magenta' { 35 }
+      'Cyan' { 36 }
+      'White' { 37 }
+      default { 0 } # Default to no color
+    }
+    "`u{001b}[${colorCode}m${Message}`u{001b}[2m${MessageDetail}`u{001b}[0m"
+  }
+}
+function Out-ColoredStdout {
+  # .SYNOPSIS
+  # 	Outputs colored text to the standard output.
+  if ($local:output = Out-Colored @args) { [Console]::Out.WriteLine($local:output)	}
+}
+function Out-ColoredStderr {
+  # .SYNOPSIS
+  # 	Outputs colored text to the standard error.
+  if ($local:output = Out-Colored @args) { [Console]::Error.WriteLine($local:output)	}
+}
+
+function Out-Help {
+  # .SYNOPSIS
+  # 	Outputs help information for the specified mode.
+  #   If mode is not specified, displays general help information.
+  param(
+    # The mode for which to display help information (e.g., 'apply', 'verify', 'fetch').
+    [string]$Mode
+  )
+  switch ($Mode) {
+    'apply' {
+      Write-Host @'
+Usage: yana.ps1 apply [options]
+  Applies the specified YANA Module.
+
+  Options:
+  [-source] <path|url>   Specifies the source of YANA Module to apply.
+'@
+    }
+    'verify' {
+      Write-Host @'
+Usage: yana.ps1 verify [options]
+
+  Options:
+  [-source] <path|url>   Specifies the source of YANA Module to verify.
+'@
+    }
+    'fetch' {
+      Write-Host @'
+Usage: yana.ps1 fetch [options]
+
+  Options:
+  [-source] <path|url>   Specifies the source of YANA Module to fetch.
+'@
+    }
+    default {
+      Write-Host @'
+Usage: yana.ps1 <general options> [mode] <mode options>
+
+Modes:
+  apply                   Applies the specified YANA Module.
+  verify                  Compares the state of the system with the state specified by the YANA Module without making any changes.
+  fetch                   Fetches the specified YANA Module.
+
+General Options:
+  -version               Displays the version of YANA.
+  -help                  Displays this help message.
+  -help <mode>           Displays help for the specified mode.
+  -logfile <path>        Specifies the log file path. Uses YANA_LOGFILE environment variable if set.
+  -quiet                 Suppresses output messages. Uses YANA_QUIET environment variable if set.
+  -nocolor               Disables colored output. Uses YANA_NOCOLOR environment variable if set.
+'@
+    }
+  }
+}
+
+function Invoke-YanaApply {
+  # .SYNOPSIS
+  # 	Applies the specified YANA Module.
+  param(
+    # The source of the YANA Module to apply (e.g., a file path or URL).
+    [string]$Source
+  )
+  if ([string]::IsNullOrEmpty($Source)) { throw 'Source is required for ''apply'' mode.' }
+  Out-ColoredStderr -Color 'Magenta' -Message "Applying YANA Module from source: $Source"
+  # Placeholder for actual implementation of applying the YANA module
+}
+
+function Invoke-YanaVerify {
+  # .SYNOPSIS
+  # 	Verifies the state of the system against the specified YANA Module.
+  param(
+    # The source of the YANA Module to verify (e.g., a file path or URL).
+    [string]$Source
+  )
+  if ([string]::IsNullOrEmpty($Source)) { throw 'Source is required for ''verify'' mode.' }
+  Out-ColoredStderr -Color 'Magenta' -Message "Verifying YANA Module from source: $Source"
+  # Placeholder for actual implementation of verifying the YANA module
+}
+
+function Invoke-YanaFetch {
+  # .SYNOPSIS
+  # 	Fetches the specified YANA Module.
+  param(
+    # The source of the YANA Module to fetch (e.g., a file path or URL).
+    [string]$Source
+  )
+  if ([string]::IsNullOrEmpty($Source)) { throw 'Source is required for ''fetch'' mode.' }
+  Out-ColoredStderr -Color 'Magenta' -Message "Fetching YANA Module from source: $Source"
+  # Placeholder for actual implementation of fetching the YANA module
+}
+
+function Invoke-Yana {
+  # .SYNOPSIS
+  # 	The main entry point for YANA.
+  param(
+    [switch]$Version,
+    [switch]$Help,
+    [Parameter(Position = 0)]
+    [string]$Mode = $Env:YANA_MODE,
+    [Parameter(Position = 1)]
+    [string]$Source = $Env:YANA_SOURCE,
+    # [switch]$DryRun = [bool]$Env:YANA_DRYRUN,
+    # If specified, outputs log messages to the given file.
+    # Uses YANA_LOGFILE environment variable if set.
+    [string]$LogFile = $Env:YANA_LOGFILE,
+    # If specified, suppresses output messages.
+    # Uses YANA_QUIET environment variable if set.
+    [switch]$Quiet = [bool]$Env:YANA_QUIET,
+    # If specified, disables colored output.
+    # Uses YANA_NOCOLOR environment variable if set.
+    [switch]$NoColor = [bool]$Env:YANA_NOCOLOR
+  )
+  # Disable progress bar output
+  $Global:ProgressPreference = 'SilentlyContinue'
+
+  Out-ColoredStderr -Message $YANA_TITLE -MessageDetail "Version: $YANA_VERSION"
+
+  if ($Help) { Out-Help -Mode $Mode; return }
+  if ($Version) { Write-Output $YANA_VERSION; return }
+  switch ($Mode) {
+    'apply' {
+      Invoke-YanaApply -Source $Source
+      break
+    }
+    'verify' {
+      Invoke-YanaVerify -Source $Source
+      break
+    }
+    'fetch' {
+      Invoke-YanaFetch -Source $Source
+      break
+    }
+    default {
+      Out-Help
+      throw "Invalid mode specified: '$Mode'. Use -help for usage information."
+    }
+  }
+}
+
+# Prevent running when dot-sourced
+if ($MyInvocation.InvocationName -ne '.') {
+  try {
+    Invoke-Yana @args
+  } catch {
+    Out-ColoredStderr -Color 'Red' -Message "Error: $($_.Exception.Message)" -MessageDetail $_.ScriptStackTrace
+    exit 1
+  }
+}
---
+++ yana.yanatests.ps1
@@ -0,0 +1,183 @@
+. "$PSScriptRoot/yana.ps1"
+
+function YANAtest:Invoke-Yana@invalid_mode {
+  $mode = 'invalid_mode'
+  $null = & {
+    function Out-Colored {}
+    $Script:mock_buffer = @()
+    function Out-ColoredStderr {
+      # Declare the parameters which make sense for testing purposes.
+      param(
+        [string]$Color,
+        [string]$Message,
+        [string]$MessageDetail
+      )
+      $Script:mock_buffer += $PSBoundParameters
+    }
+    try {
+      Invoke-Yana -Mode $mode
+      $Script:exit_code = 0
+    } catch {
+      # Catch the exit to prevent the script from terminating during testing
+      $Script:exit_code = $_.Exception.HResult
+    }
+  }
+  if ($Script:exit_code -eq 1) { pass 'Exit code is correct' } else { fail "Expected exit code 1 but got: $($Script:exit_code)" }
+  if ($Script:mock_buffer.Count -eq 1) { pass 'Error is displayed' } else { fail 'Should display error but got:', $Script:mock_buffer.Message }
+  if ($Script:mock_buffer[0].Color -eq 'Red') { pass 'Error color is red' } else { fail "Expected error color to be 'Red' but got: $($Script:mock_buffer[0].Color)" }
+  if ($Script:mock_buffer[0].Message -eq "Error: Invalid mode specified: '$mode'. Use -help for usage information.") {
+    pass 'Error message is correct'
+  } else { fail "Expected error message to be 'Error: Invalid mode specified: '$mode'. Use -help for usage information.' but got: $($Script:mock_buffer[0].Message)" }
+}
+
+function YANAtest:Invoke-Yana@help_no_mode {
+  $null = & {
+    function Out-Colored {}
+    $Script:mock_buffer = @()
+    function Out-Help {
+      param([string]$Mode)
+      $Script:mock_buffer += $PSBoundParameters
+    }
+    try {
+      Invoke-Yana -help
+      $Script:exit_code = 0
+    } catch {
+      # Catch the exit to prevent the script from terminating during testing
+      $Script:exit_code = $_.Exception.HResult
+    }
+  }
+  if ($Script:exit_code -eq 0) {
+    pass 'Exit code is 0'
+  } else {
+    fail 'Expected exit code 0 but got:', $Script:exit_code
+  }
+  if ($Script:mock_buffer.Count -eq 1) {
+    pass 'Out-Help is called'
+  } else {
+    fail "Expected Out-Help to be called but got: $($Script:mock_buffer.Count) calls"
+  }
+  if ([string]::IsNullOrEmpty($Script:mock_buffer[0].Mode)) {
+    pass 'Out-Help mode is empty'
+  } else {
+    fail "Expected Out-Help mode to be empty but got: $($Script:mock_buffer[0].Mode)"
+  }
+}
+
+function YANAtest:Invoke-Yana@help_with_mode {
+  $mode = 'apply'
+  $null = & {
+    function Out-Colored {}
+    $Script:mock_buffer = @()
+    function Out-Help {
+      param([string]$Mode)
+      $Script:mock_buffer += $PSBoundParameters
+    }
+    try {
+      Invoke-Yana -help -Mode $mode
+      $Script:exit_code = 0
+    } catch {
+      # Catch the exit to prevent the script from terminating during testing
+      $Script:exit_code = $_.Exception.HResult
+    }
+  }
+  if ($Script:exit_code -eq 0) { pass 'Exit code is correct' } else { fail "Expected exit code 0 but got: $($Script:exit_code)" }
+  if ($Script:mock_buffer.Count -eq 1) { pass 'Out-Help is called' } else { fail "Expected Out-Help to be called but got: $($Script:mock_buffer.Count) calls" }
+  if ($Script:mock_buffer[0].Mode -eq $mode) { pass 'Out-Help mode is correct' } else { fail "Expected Out-Help mode to be '$mode' but got: $($Script:mock_buffer[0].Mode)" }
+}
+
+function YANAtest:Invoke-Yana@version {
+  function Out-Colored {}
+  $test_result = Invoke-Yana -Version
+  if ($test_result -eq $YANA_VERSION) { pass 'Version output is correct' } else { fail "Expected version output to be '$YANA_VERSION' but got: $test_result" }
+}
+
+function YANAtest:Invoke-Yana@mode_apply {
+  $source = 'test_source'
+  $null = & {
+    function Out-Colored {}
+    $Script:mock_buffer = @()
+    function Invoke-YanaApply {
+      param([string]$Source)
+      $Script:mock_buffer += $PSBoundParameters
+    }
+    try {
+      Invoke-Yana -Mode 'apply' -Source $source
+    } catch {
+      fail 'Caught exception:', $_.Exception.Message
+    }
+  }
+  if ($Script:mock_buffer.Count -eq 1) { pass 'Invoke-YanaApply is called' } else { fail "Expected Invoke-YanaApply to be called but got: $($Script:mock_buffer.Count) calls" }
+  if ($Script:mock_buffer[0].Source -eq $source) { pass 'Invoke-YanaApply source is correct' } else { fail "Expected Invoke-YanaApply source to be '$source' but got: $($Script:mock_buffer[0].Source)" }
+}
+function YANAtest:Invoke-Yana@mode_verify {
+  $source = 'test_source'
+  $null = & {
+    function Out-Colored {}
+    $Script:mock_buffer = @()
+    function Invoke-YanaVerify {
+      param([string]$Source)
+      $Script:mock_buffer += $PSBoundParameters
+    }
+    try {
+      Invoke-Yana -Mode 'verify' -Source $source
+    } catch {
+      fail 'Caught exception:', $_.Exception.Message
+    }
+  }
+  if ($Script:mock_buffer.Count -eq 1) { pass 'Invoke-YanaVerify is called' } else { fail "Expected Invoke-YanaVerify to be called but got: $($Script:mock_buffer.Count) calls" }
+  if ($Script:mock_buffer[0].Source -eq $source) { pass 'Invoke-YanaVerify source is correct' } else { fail "Expected Invoke-YanaVerify source to be '$source' but got: $($Script:mock_buffer[0].Source)" }
+}
+function YANAtest:Invoke-Yana@mode_fetch {
+  $source = 'test_source'
+  $null = & {
+    function Out-Colored {}
+    $Script:mock_buffer = @()
+    function Invoke-YanaFetch {
+      param([string]$Source)
+      $Script:mock_buffer += $PSBoundParameters
+    }
+    try {
+      Invoke-Yana -Mode 'fetch' -Source $source
+    } catch {
+      fail 'Caught exception:', $_.Exception.Message
+    }
+  }
+  if ($Script:mock_buffer.Count -eq 1) { pass 'Invoke-YanaFetch is called' } else { fail "Expected Invoke-YanaFetch to be called but got: $($Script:mock_buffer.Count) calls" }
+  if ($Script:mock_buffer[0].Source -eq $source) { pass 'Invoke-YanaFetch source is correct' } else { fail "Expected Invoke-YanaFetch source to be '$source' but got: $($Script:mock_buffer[0].Source)" }
+}
+function YANAtest:Invoke-Yana@invalid_mode {
+  $mode = 'invalid_mode'
+  $null = & {
+    function Out-Colored {}
+    $Script:mock_buffer = @()
+    function Out-Help {
+      param(
+        [string]$Mode
+      )
+      $Script:mock_buffer += $PSBoundParameters
+    }
+    try {
+      Invoke-Yana -Mode $mode
+      $Script:exit_code = 0
+    } catch {
+      # Catch the exit to prevent the script from terminating during testing
+      $Script:exit_code = $_.Exception.HResult
+    }
+  }
+  if ($Script:exit_code -ne 0) {
+    pass 'Exit code is correct'
+  } else {
+    fail "Expected exit code non-zero but got: $($Script:exit_code)"
+  }
+  if ($Script:mock_buffer.Count -eq 1) {
+    pass 'Out-Help is called'
+  } else {
+    fail 'Should call Out-Help 1 time but got:', $Script:mock_buffer.Count
+  }
+  if ([string]::IsNullOrEmpty($Script:mock_buffer[0].Mode)) {
+    pass 'Out-Help mode is empty'
+  } else {
+    fail 'Expected Out-Help mode to be empty but got:', $Script:mock_buffer[0].Mode
+  }
+
+}
