450de840833861508aed888f376a31469fb4c149
plechenko <pavel.lechenko@gmail.com>
2026-07-27 22:08:11 +0200

    refactor: update function names and improve error handling in YANA tests

--- yana.sh
+++ yana.sh
@@ -12,156 +12,158 @@ fi
 [[ -z ${YANA_TITLE:-} ]] && builtin readonly YANA_TITLE='YANA - Yet Another Node Automator (Bash)'
 [[ -z ${YANA_VERSION:-} ]] && builtin readonly YANA_VERSION='YANAVERSIONPLACEHOLDER'
 
-# Prepares colored text for output to the console.
-# Takes care of logging to a file if $YANA_LOGFILE is specified.
-# If $YANA_QUIET is specified, suppresses output.
-# If $YANA_NOCOLOR is specified, disables colored output.
-function out_colored() {
-	builtin local Color="${1:-${Color:-}}"
-	builtin local Message="${2:-${Message:-}}"
-	builtin local MessageDetail="${3:-${MessageDetail:-}}"
-
-	[[ -n $Message ]] && Message="$Message "
-	if [[ -n $YANA_LOGFILE ]]; then
-		builtin local logMessage
-		logMessage="[$(date -u +'%Y-%m-%dT%H:%M:%SZ')] ${Message}${MessageDetail}"
-		builtin echo -e "${logMessage}" >>"$YANA_LOGFILE" || {
-			builtin local _logfile="$YANA_LOGFILE"
-			YANA_LOGFILE=""
-			out_colored red 'Error: Failed to write to log file' "$_logfile" >&2
-		}
-	fi
-
-	[[ -n $YANA_QUIET ]] && builtin return
-	if [[ -n $YANA_NOCOLOR ]]; then
-		Message="${Message}${MessageDetail}"
-	else
-		builtin local ColorCode
-		case "$Color" in
-		black) ColorCode=30 ;;
-		red) ColorCode=31 ;;
-		green) ColorCode=32 ;;
-		yellow) ColorCode=33 ;;
-		blue) ColorCode=34 ;;
-		magenta) ColorCode=35 ;;
-		cyan) ColorCode=36 ;;
-		white) ColorCode=37 ;;
-		*) ColorCode=0 ;;
-		esac
-		Message="\e[${ColorCode}m${Message}\e[2m${MessageDetail}\e[0m"
-	fi
-	builtin echo "$Message"
-}
-# Outputs colored text to the standard output.
-function out_colored_stdout() {
-	output="$(out_colored "$@")"
-	[[ -n $output ]] && builtin echo -e "$output" >&1
-}
-# Outputs colored text to the standard error.
-function out_colored_stderr() {
-	output="$(out_colored "$@")"
-	[[ -n $output ]] && builtin echo -e "$output" >&2
-}
-# Throws an error message and exits the script with exit code 1.
-function throw() {
-	builtin local Message="${1:-${Message:-}}"
-	builtin local MessageDetail="${2:-${MessageDetail:-}}"
-	out_colored_stderr red "Error: $Message" "$MessageDetail"
-	builtin exit 1
-}
-
-function invoke_yana_fetch() {
-	builtin local source="${1:-}"
-	[[ -n $source ]] || throw 'Missing source for fetch'
-	out_colored_stderr cyan "Fetching YANA Module" "$source"
-	# Implement the fetch logic here
-}
-
-function invoke_yana_apply() {
-	builtin local source="${1:-}"
-	[[ -n $source ]] || throw 'Missing source for apply'
-	out_colored_stderr cyan "Applying YANA Module" "$source"
-	# Implement the apply logic here
-}
-
-function invoke_yana_verify() {
-	builtin local source="${1:-}"
-	[[ -n $source ]] || throw 'Missing source for verify'
-	out_colored_stderr cyan "Verifying YANA Module" "$source"
-	# Implement the verify logic here
-}
-
-function out_help() {
-	builtin local mode="${1:-}"
-	case "$mode" in
+_yana_usage() {
+	case "${YANA_MODE:-}" in
 	apply)
-		builtin echo "Usage: yana.sh apply <mode options>"
+		builtin echo "Usage: yana.sh apply -source <path|url> [-routine <name>]"
 		builtin echo "  Applies the specified YANA Module."
-
 		builtin echo "Options:"
-		builtin echo "  -source <path|url>         Specifies the source of YANA Module to apply."
+		builtin echo "  -source <path|url>         Specifies the source of YANA Module to apply. Can be a local path or a URL. Uses YANA_LOGFILE environment variable."
+		builtin echo "  -routine <name>            Specifies the routine to execute within the YANA Module."
 		;;
 	verify)
-		builtin echo "Usage: yana.sh verify <mode options>"
+		builtin echo "Usage: yana.sh verify -source <path|url> [-routine <name>]"
 		builtin echo "  Compares the state of the system with the state specified by the YANA Module without making any changes."
-
 		builtin echo "Options:"
-		builtin echo "  -source <path|url>         Specifies the source of YANA Module to verify."
+		builtin echo "  -source <path|url>         Specifies the source of YANA Module to verify. Can be a local path or a URL. Uses YANA_LOGFILE environment variable."
+		builtin echo "  -routine <name>            Specifies the routine to execute within the YANA Module."
 		;;
 	fetch)
-		builtin echo "Usage: yana.sh fetch <mode options>"
-		builtin echo "  Fetches the specified YANA Module."
-
+		builtin echo "Usage: yana.sh fetch -source <path|url>"
+		builtin echo "  Fetches the specified YANA Module from the given source (path or URL)."
 		builtin echo "Options:"
-		builtin echo "  -source <path|url>         Specifies the source of YANA Module to fetch."
+		builtin echo "  -source <path|url>         Specifies the source of YANA Module to fetch. Can be path or URL. Uses YANA_LOGFILE environment variable."
+		;;
+	version)
+		builtin echo "Usage: yana.sh version"
+		builtin echo "  Displays the version of YANA."
 		;;
 	*)
 		builtin echo "Usage: yana.sh <general options> [mode] <mode options>"
-		builtin echo
 		builtin echo "Modes:"
+		builtin echo "  version									 	 Displays the version of YANA."
 		builtin echo "  apply                      Applies the specified YANA Module."
 		builtin echo "  verify                     Compares the state of the system with the state specified by the YANA Module without making any changes."
 		builtin echo "  fetch                      Fetches the specified YANA Module."
 		;;
 	esac
-	builtin echo
 	builtin echo "General Options:"
-	builtin echo "  -version                   Displays the version of YANA."
 	builtin echo "  -help                      Displays this help message."
 	builtin echo "  -help <mode>               Displays help for the specified mode."
 	builtin echo "  -logfile <file>            Log file path. Uses YANA_LOGFILE environment variable. If not specified, logs are not written to a file."
-	builtin echo "  -quiet                     Suppress output to the console. Uses YANA_QUIET environment variable."
-	builtin echo "  -nocolor                   Disable colored output. Uses YANA_NOCOLOR environment variable."
 }
 
+# Logs a message to the stderr.
+# Takes care of logging to a file if $YANA_LOGFILE is specified.
+log() {
+	builtin local _level="${1:-${level:-info}}"
+	builtin local _message="${2:-${message:-}}"
+	builtin local _logMessage
+	_logMessage="$(date -u +'%Y-%m-%dT%H:%M:%SZ')\t${_level^^}\t${_message}"
+	builtin echo -e "$_logMessage" >&2
+	if [[ -n $YANA_LOGFILE ]]; then
+		builtin echo -e "${_logMessage}" >>"$YANA_LOGFILE" || throw "Failed to write to log file '$YANA_LOGFILE'. Check permissions and available disk space."
+	fi
+}
+
+# Throws an error message and exits the script with the specified return code.
+builtin readonly ERR_GENERAL=1 ERR_MISUSE=64 ERR_DATA_FORMAT=65 ERR_NO_INPUT=66
+throw() {
+	builtin local _message="${1:-${message:-}}"
+	builtin local _rc="${2:-${rc:-$ERR_GENERAL}}"
+	log fatal "$_message"
+	builtin local _frame=0
+	while true; do
+		trace=$(builtin caller $_frame | awk '{print $3 ":" $1 " (" $2 ") "}') || builtin break
+		log trace "$trace"
+		((_frame += 1))
+	done
+	builtin exit "$_rc"
+}
+
+_yana_check_prerequisites() {
+	builtin local cmd
+	for cmd in "$@"; do builtin command -v "$cmd" &>/dev/null || throw "Prerequisite tool '$cmd' is missing on host node." $ERR_MISUSE; done
+}
+# Resolves variable placeholders in the input string using the provided context (param, env, var, output).
+_yana_resolve_vars() {
+	builtin local _input="${1:-}"
+	builtin local _resolve_iters=0 _max_iters=${max_iterations:-50}
+	while [[ $_input =~ \$\{(param|env|var|output):([a-zA-Z0-9_]+)\} ]]; do
+		((_resolve_iters++))
+		[[ $_resolve_iters -gt $_max_iters ]] && throw "Variable resolution exceeded $_max_iters iterations (possible circular reference)." $ERR_DATA_FORMAT
+		builtin local _var="${BASH_REMATCH[0]}" _ctx="${BASH_REMATCH[1]}" _key="${BASH_REMATCH[2]}" _value=''
+		case "$_ctx" in
+		param) _value="${_yana_spec_params[$_key]:-}" ;;
+		output) _value="${_yana_outputs[$_key]:-}" ;;
+		env) builtin declare -px "$_key" &>/dev/null && _value="${!_key:-}" ;;
+		var) builtin declare -F "yanavar_${_key}" &>/dev/null && _value="$(yanavar_"${_key}")" ;;
+		*) throw "Unknown variable type '$_ctx' in variable reference '$_var'. This should never happen. Please report this as a bug." $ERR_GENERAL ;;
+		esac
+		[[ -z $_value ]] && log warning "Variable '$_var' resolved to an empty value. Ensure that the variable is defined and has a non-empty value."
+		_input="${_input//$_var/$_value}"
+	done
+	builtin echo "$_input"
+}
+
+_yana_read_spec_file() {
+	local _yana_spec_file="${YANA_SOURCE}/${YANA_ROUTINE}.yana.json"
+
+	# [[ -n $_yana_spec_file ]] || yana_throw "No spec file provided." $ERR_MISUSE
+	# [[ -f $_yana_spec_file ]] || yana_throw "Spec file '$_yana_spec_file' not found." $ERR_NO_INPUT
+
+	jq -c -r '.' "$_yana_spec_file" 2>/dev/null || yana_throw "Failed to parse YANA spec file '$_yana_spec_file'. Ensure it is valid JSON." $ERR_DATA_FORMAT
+}
+
+# Outputs the version of YANA.
+_yana_mode_version() { builtin echo "$YANA_VERSION"; }
+# Fetches and unpacks the YANA Module from the specified source (local path or URL).
+_yana_mode_fetch() {
+	[[ -z $YANA_SOURCE ]] && throw 'No source specified'
+	log info "Fetching YANA Module: $YANA_SOURCE"
+
+	# If the source is a URL, download it to the cache directory.
+
+
+}
+_yana_mode_verify() {
+	[[ -z $YANA_SOURCE ]] && throw 'No source specified'
+	log info "Verifying YANA Module: $YANA_SOURCE"
+	# Implement the verify logic here
+}
+_yana_mode_apply() {
+	[[ -z $YANA_SOURCE ]] && throw 'No source specified'
+	# _yana_mode_fetch
+	log info "Applying YANA Module: $YANA_SOURCE"
+	# Implement the apply logic here
+}
 # Parse command-line arguments and set global variables accordingly.
-function parse_args() {
+_yana_parse_args() {
 	while [[ $# -gt 0 ]]; do
 		case "$1" in
-		apply | verify | fetch | -version)
+		apply | verify | fetch | version)
 			YANA_MODE="$1"
 			;;
-		-source)
+		-source | --source)
 			builtin shift
 			[[ $# -ge 1 && $1 != -* ]] || throw 'Missing value for -source'
 			YANA_SOURCE="$1"
 			;;
-		-logfile)
+		-routine | --routine)
+			builtin shift
+			[[ $# -ge 1 && $1 != -* ]] || throw 'Missing value for -routine'
+			YANA_ROUTINE="$1"
+			;;
+		-logfile | --logfile)
 			builtin shift
 			[[ $# -ge 1 && $1 != -* ]] || throw 'Missing value for -logfile'
 			YANA_LOGFILE="$1"
 			;;
-		-quiet)
-			YANA_QUIET=true
-			;;
-		-nocolor)
-			YANA_NOCOLOR=true
-			;;
-		-help)
-			YANA_SHOW_HELP=true
+		-help | --help)
+			_yana_show_help=true
 			;;
 		*)
-			if [[ "$1" == -* ]]; then
+			if [[ $1 == -* ]]; then
 				throw "Unknown option: $1. Use -help to see available options."
 			fi
 			throw "Unknown mode: $1. Use -help to see available modes."
@@ -170,34 +172,26 @@ function parse_args() {
 		builtin shift
 	done
 }
-
 # Main entry point.
-function invoke_yana() {
-	YANA_MODE="${YANA_MODE:-}"
-	YANA_SOURCE="${YANA_SOURCE:-}"
-	YANA_LOGFILE="${YANA_LOGFILE:-}"
-	YANA_QUIET="${YANA_QUIET:-}"
-	YANA_NOCOLOR="${YANA_NOCOLOR:-}"
-	YANA_SHOW_HELP=false
-	parse_args "$@"
+_yana_() {
+	builtin local YANA_MODE="${YANA_MODE:-}" YANA_SOURCE="${YANA_SOURCE:-}" YANA_ROUTINE="${YANA_ROUTINE:-}" YANA_LOGFILE="${YANA_LOGFILE:-}" _yana_show_help=false
+	_yana_parse_args "$@"
 
-	out_colored_stderr '' "$YANA_TITLE" "Version: $YANA_VERSION"
+	# Display the title and version information
+	log info "$YANA_TITLE" "Version: $YANA_VERSION" >&2
+	_yana_check_prerequisites jq base64 awk
 
-	if [[ $YANA_MODE == '-version' ]]; then
-		builtin echo "$YANA_VERSION"
+	if [[ $_yana_show_help == true ]]; then
+		_yana_usage
 		builtin return 0
 	fi
-	if [[ $YANA_SHOW_HELP == true ]]; then
-		out_help "$YANA_MODE"
-		builtin return 0
-	fi
-	if [[ -z $YANA_MODE ]]; then
-		throw 'No mode specified. Use -help to see available modes.'
-	fi
-	invoke_yana_"$YANA_MODE" "$YANA_SOURCE"
+	[[ -z $YANA_MODE ]] && throw 'No mode specified. Use -help to see available modes.'
+	builtin local YANA_CACHE_DIR="${YANA_CACHE_DIR:-$HOME/.yana/cache}" YANA_TEMP_DIR="${YANA_TEMP_DIR:-$HOME/.yana/temp}"
+	[[ -d $YANA_CACHE_DIR ]] || mkdir -p "$YANA_CACHE_DIR" || throw "Failed to create cache directory '$YANA_CACHE_DIR'." $ERR_GENERAL
+	[[ -d $YANA_TEMP_DIR ]] || mkdir -p "$YANA_TEMP_DIR" || throw "Failed to create temp directory '$YANA_TEMP_DIR'." $ERR_GENERAL
+	_yana_mode_"$YANA_MODE"
 }
-
 if [[ -z ${BASH_SOURCE[1]:-} ]] || [[ ${BASH_SOURCE[1]:-bashdb} == *bashdb ]]; then
 	# Proceed with the script execution only if it is executed directly or under bashdb.
-	invoke_yana "$@" || return $?
+	_yana_ "$@" || builtin exit $?
 fi
--- yana.yanatests.sh
+++ yana.yanatests.sh
@@ -2,58 +2,73 @@
 
 . "${BASH_SOURCE[0]%/*}/yana.sh"
 
-function YANAtest:invoke_yana@no_args {
+function YANAtest:_yana_@no_args {
 	local _rc test_result
 	_rc=0
-	test_result=$(YANA_MODE='' YANA_SOURCE='' YANA_LOGFILE='' YANA_QUIET='' YANA_NOCOLOR='' invoke_yana 2>&1) || _rc=$?
+	test_result=$(
+		throw() {
+			builtin echo "Thrown: $1"
+			builtin exit 1
+		}
+		YANA_MODE='' YANA_LOGFILE='' _yana_ 2>/dev/null) || _rc=$?
 	if [[ $_rc -eq 1 ]]; then
 		pass 'should return 1 when invoked with no mode'
 	else
 		fail "should return 1 when invoked with no mode, got: $_rc"
 	fi
-	if [[ $test_result == *'Error: No mode specified. Use -help to see available modes.'* ]]; then
+	if [[ $test_result == 'Thrown: No mode specified. Use -help to see available modes.' ]]; then
 		pass 'should print error message when invoked with no mode'
 	else
 		fail "should print error message when invoked with no mode, got: $test_result"
 	fi
 }
 
-function YANAtest:invoke_yana@unknown_mode {
+function YANAtest:_yana_@unknown_mode {
 	local _rc test_result
 	_rc=0
-	test_result=$(YANA_MODE='' YANA_SOURCE='' YANA_LOGFILE='' YANA_QUIET='' YANA_NOCOLOR='' invoke_yana invalid_mode 2>&1) || _rc=$?
+	test_result=$(
+				throw() {
+			builtin echo "Thrown: $1"
+			builtin exit 1
+		}
+		YANA_MODE='' YANA_LOGFILE='' _yana_ invalid_mode 2>&1) || _rc=$?
 	if [[ $_rc -eq 1 ]]; then
 		pass 'should return 1 when invoked with unknown mode'
 	else
 		fail "should return 1 when invoked with unknown mode, got: $_rc"
 	fi
-	if [[ $test_result == *'Error: Unknown mode: invalid_mode. Use -help to see available modes.'* ]]; then
-		pass 'should print error message when invoked with unknown mode'
+	if [[ $test_result == 'Thrown: Unknown mode: invalid_mode. Use -help to see available modes.' ]]; then
+		pass 'should throw error message when invoked with unknown mode'
 	else
-		fail "should print error message when invoked with unknown mode, got: $test_result"
+		fail "should throw error message when invoked with unknown mode, got: $test_result"
 	fi
 }
 
-function YANAtest:invoke_yana@unknown_option {
+function YANAtest:_yana_@unknown_option {
 	local _rc test_result
 	_rc=0
-	test_result=$(YANA_MODE='' YANA_SOURCE='' YANA_LOGFILE='' YANA_QUIET='' YANA_NOCOLOR='' invoke_yana -invalid_option 2>&1) || _rc=$?
+	test_result=$(
+		throw() {
+			builtin echo "Thrown: $1"
+			builtin exit 1
+		}
+		YANA_LOGFILE='' _yana_ -invalid_option) || _rc=$?
 	if [[ $_rc -eq 1 ]]; then
 		pass 'should return 1 when invoked with unknown option'
 	else
 		fail "should return 1 when invoked with unknown option, got: $_rc"
 	fi
-	if [[ $test_result == *'Error: Unknown option: -invalid_option. Use -help to see available options.'* ]]; then
-		pass 'should print error message when invoked with unknown option'
+	if [[ $test_result == 'Thrown: Unknown option: -invalid_option. Use -help to see available options.' ]]; then
+		pass 'should throw error message when invoked with unknown option'
 	else
-		fail "should print error message when invoked with unknown option, got: $test_result"
+		fail "should throw error message when invoked with unknown option, got: $test_result"
 	fi
 }
 
-function YANAtest:invoke_yana@help {
+function YANAtest:_yana_@help {
 	local _rc test_result
 	_rc=0
-	test_result=$(YANA_MODE='' YANA_SOURCE='' YANA_LOGFILE='' YANA_QUIET='' YANA_NOCOLOR='' invoke_yana -help 2>&1) || _rc=$?
+	test_result=$(YANA_MODE='' YANA_SOURCE='' YANA_LOGFILE='' _yana_ -help 2>&1) || _rc=$?
 	if [[ $_rc -eq 0 ]]; then
 		pass 'should return 0 when invoked with -help'
 	else
@@ -66,129 +81,136 @@ function YANAtest:invoke_yana@help {
 	fi
 }
 
-function YANAtest:invoke_yana@help_mode_apply {
+function YANAtest:_yana_@help_mode_apply {
 	local _rc test_result
 	_rc=0
-	test_result=$(YANA_MODE='' YANA_SOURCE='' YANA_LOGFILE='' YANA_QUIET='' YANA_NOCOLOR='' invoke_yana -help apply 2>&1) || _rc=$?
+	test_result=$(YANA_MODE='' YANA_SOURCE='' YANA_LOGFILE='' _yana_ -help apply 2>&1) || _rc=$?
 	if [[ $_rc -eq 0 ]]; then
 		pass 'should return 0 when invoked with -help apply'
 	else
 		fail "should return 0 when invoked with -help apply, got: $_rc"
 	fi
-	if [[ $test_result == *'Usage: yana.sh apply <mode options>'* ]]; then
+	if [[ $test_result == *'Usage: yana.sh apply -source <path|url> [-routine <name>]'* ]]; then
 		pass 'should print usage information for apply mode when invoked with -help apply'
 	else
 		fail "should print usage information for apply mode when invoked with -help apply, got: $test_result"
 	fi
 }
 
-function YANAtest:invoke_yana@help_mode_verify {
+function YANAtest:_yana_@help_mode_verify {
 	local _rc test_result
 	_rc=0
-	test_result=$(YANA_MODE='' YANA_SOURCE='' YANA_LOGFILE='' YANA_QUIET='' YANA_NOCOLOR='' invoke_yana -help verify 2>&1) || _rc=$?
+	test_result=$(YANA_MODE='' YANA_SOURCE='' YANA_LOGFILE='' _yana_ -help verify 2>&1) || _rc=$?
 	if [[ $_rc -eq 0 ]]; then
 		pass 'should return 0 when invoked with -help verify'
 	else
 		fail "should return 0 when invoked with -help verify, got: $_rc"
 	fi
-	if [[ $test_result == *'Usage: yana.sh verify <mode options>'* ]]; then
+	if [[ $test_result == *'Usage: yana.sh verify -source <path|url> [-routine <name>]'* ]]; then
 		pass 'should print usage information for verify mode when invoked with -help verify'
 	else
 		fail "should print usage information for verify mode when invoked with -help verify, got: $test_result"
 	fi
 }
 
-function YANAtest:invoke_yana@help_mode_fetch {
+function YANAtest:_yana_@help_mode_fetch {
 	local _rc test_result
 	_rc=0
-	test_result=$(YANA_MODE='' YANA_SOURCE='' YANA_LOGFILE='' YANA_QUIET='' YANA_NOCOLOR='' invoke_yana -help fetch 2>&1) || _rc=$?
+	test_result=$(YANA_MODE='' YANA_SOURCE='' YANA_LOGFILE='' _yana_ -help fetch 2>&1) || _rc=$?
 	if [[ $_rc -eq 0 ]]; then
 		pass 'should return 0 when invoked with -help fetch'
 	else
 		fail "should return 0 when invoked with -help fetch, got: $_rc"
 	fi
-	if [[ $test_result == *'Usage: yana.sh fetch <mode options>'* ]]; then
+	if [[ $test_result == *'Usage: yana.sh fetch -source <path|url>'* ]]; then
 		pass 'should print usage information for fetch mode when invoked with -help fetch'
 	else
 		fail "should print usage information for fetch mode when invoked with -help fetch, got: $test_result"
 	fi
 }
 
-function YANAtest:invoke_yana@mode_apply {
+function YANAtest:_yana_@mode_apply {
 	local _rc test_result
 	_rc=0
 	test_result=$(
-		function invoke_yana_apply() {
-			builtin echo "apply: '$1'"
+		function _yana_mode_apply() {
+			builtin echo "apply: '$YANA_SOURCE' '$YANA_ROUTINE'"
 		}
 
-		YANA_MODE='' YANA_SOURCE='' YANA_LOGFILE='' YANA_QUIET='' YANA_NOCOLOR='' invoke_yana apply -source 'qwerty' 2>/dev/null
+		YANA_MODE='' YANA_SOURCE='' YANA_LOGFILE='' _yana_ apply -source 'qwerty' -routine 'hello' 2>/dev/null
 	) || _rc=$?
 	if [[ $_rc -eq 0 ]]; then
 		pass 'should return 0 when invoked with apply'
 	else
 		fail "should return 0 when invoked with apply, got: $_rc"
 	fi
-	if [[ $test_result == "apply: 'qwerty'" ]]; then
+	if [[ $test_result == "apply: 'qwerty' 'hello'" ]]; then
 		pass 'should print apply mode invocation message'
 	else
 		fail "should print apply mode invocation message, got: $test_result"
 	fi
 }
 
-function YANAtest:invoke_yana@mode_apply_env {
+function YANAtest:_yana_@mode_apply_env {
 	local _rc test_result
 	_rc=0
 	test_result=$(
-		function invoke_yana_apply() {
-			builtin echo "apply: '$1'"
+		function _yana_mode_apply() {
+			builtin echo "apply: '$YANA_SOURCE' '$YANA_ROUTINE'"
 		}
 
-		YANA_MODE='' YANA_SOURCE='' YANA_LOGFILE='' YANA_QUIET='' YANA_NOCOLOR='' YANA_MODE=apply YANA_SOURCE='qwerty' invoke_yana 2>/dev/null
+		YANA_LOGFILE='' YANA_MODE=apply YANA_SOURCE='qwerty' YANA_ROUTINE='hello' _yana_ 2>/dev/null
 	) || _rc=$?
 	if [[ $_rc -eq 0 ]]; then
 		pass 'should return 0 when invoked with apply'
 	else
 		fail "should return 0 when invoked with apply, got: $_rc"
 	fi
-	if [[ $test_result == "apply: 'qwerty'" ]]; then
+	if [[ $test_result == "apply: 'qwerty' 'hello'" ]]; then
 		pass 'should print apply mode invocation message'
 	else
 		fail "should print apply mode invocation message, got: $test_result"
 	fi
 }
 
-function YANAtest:invoke_yana@mode_apply_no_source {
+function YANAtest:_yana_@mode_apply_no_source {
 	local _rc test_result
 	_rc=0
 	test_result=$(
-		function invoke_yana_apply() {
-			builtin echo "apply: '$1'"
+		# function _yana_mode_apply() {
+		# 	builtin echo "apply: '$YANA_SOURCE' '$YANA_ROUTINE'"
+		# }
+		throw() {
+			builtin echo "Thrown: $1"
+			builtin exit 1
 		}
 
-		YANA_MODE='' YANA_SOURCE='' YANA_LOGFILE='' YANA_QUIET='' YANA_NOCOLOR='' invoke_yana apply 2>/dev/null
+		YANA_MODE='' YANA_SOURCE='' YANA_LOGFILE='' _yana_ apply 2>/dev/null
 	) || _rc=$?
-	if [[ $_rc -eq 0 ]]; then
-		pass 'should return 0 when invoked with apply'
+	if [[ $_rc -eq 1 ]]; then
+		pass 'should return 1 when invoked with apply and no source'
 	else
-		fail "should return 0 when invoked with apply, got: $_rc"
+		fail "should return 1 when invoked with apply and no source, got: $_rc"
 	fi
-	if [[ $test_result == "apply: ''" ]]; then
-		pass 'should print apply mode invocation message'
+	if [[ $test_result == 'Thrown: No source specified' ]]; then
+		pass 'should throw an error when invoked with apply and no source'
 	else
-		fail "should print apply mode invocation message, got: $test_result"
+		fail "should throw an error when invoked with apply and no source, got: $test_result"
 	fi
 }
 
-function YANAtest:invoke_yana@mode_fetch {
+function YANAtest:_yana_@mode_fetch {
 	local _rc test_result
 	_rc=0
 	test_result=$(
-		function invoke_yana_fetch() {
-			builtin echo "fetch: '$1'"
+		function _yana_mode_fetch() {
+			builtin echo "fetch: '$YANA_SOURCE'"
 		}
+		# log() {
+		# 	builtin echo "log: $1"
+		# }
 
-		YANA_MODE='' YANA_SOURCE='' YANA_LOGFILE='' YANA_QUIET='' YANA_NOCOLOR='' invoke_yana fetch -source 'qwerty' 2>/dev/null
+		YANA_MODE='' YANA_SOURCE='' YANA_LOGFILE='' _yana_ fetch -source 'qwerty' 2>/dev/null
 	) || _rc=$?
 	if [[ $_rc -eq 0 ]]; then
 		pass 'should return 0 when invoked with fetch'
@@ -202,22 +224,22 @@ function YANAtest:invoke_yana@mode_fetch {
 	fi
 }
 
-function YANAtest:invoke_yana@mode_verify {
+function YANAtest:_yana_@mode_verify {
 	local _rc test_result
 	_rc=0
 	test_result=$(
-		function invoke_yana_verify() {
-			builtin echo "verify: '$1'"
+		function _yana_mode_verify() {
+			builtin echo "verify: '$YANA_SOURCE' '$YANA_ROUTINE'"
 		}
 
-		YANA_MODE='' YANA_SOURCE='' YANA_LOGFILE='' YANA_QUIET='' YANA_NOCOLOR='' invoke_yana verify -source 'qwerty' 2>/dev/null
+		YANA_LOGFILE='' YANA_MODE='' YANA_SOURCE='' YANA_ROUTINE='' _yana_ verify -source qwerty -routine hello 2>/dev/null
 	) || _rc=$?
 	if [[ $_rc -eq 0 ]]; then
 		pass 'should return 0 when invoked with verify'
 	else
 		fail "should return 0 when invoked with verify, got: $_rc"
 	fi
-	if [[ $test_result == "verify: 'qwerty'" ]]; then
+	if [[ $test_result == "verify: 'qwerty' 'hello'" ]]; then
 		pass 'should print verify mode invocation message'
 	else
 		fail "should print verify mode invocation message, got: $test_result"
