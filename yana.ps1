fa690e6824feabd3a474d39de004ea89831703e0
plechenko <pavel.lechenko@gmail.com>
2026-08-01 18:59:17 +0200

    refactor: enhance logging functionality with color-coded levels and improve usage messages for commands

--- yana.ps1
+++ yana.ps1
@@ -7,6 +7,30 @@
 Set-Variable -Name YANA_TITLE -Value 'YANA - Yet Another Node Automator (PowerShell)' -Option Constant -Scope Script -ErrorAction:Ignore
 Set-Variable -Name YANA_VERSION -Value 'YANAVERSIONPLACEHOLDER' -Option Constant -Scope Script -ErrorAction:Ignore
 
+function log([string]$Level, [string]$Message) {
+  if ($Level -eq 'trace' -and [bool]${-Trace} -eq $false) { return }
+  if ($Level -eq 'debug' -and [bool]${-Debug} -eq $false -and [bool]${-Trace} -eq $false) { return }
+  $logMessage = "[$([datetime]::UtcNow.ToString('yyyy-MM-ddTHH:mm:ssZ'))]`t$($Level.ToUpper())`t$Message"
+  try {
+    if ($level -in @('trace', 'debug')) { [Console]::ForegroundColor = [ConsoleColor]::DarkGray }
+    elseif ($level -eq 'info') { [Console]::ForegroundColor = [ConsoleColor]::Cyan }
+    elseif ($level -in @('ok', 'success', 'pass')) { [Console]::ForegroundColor = [ConsoleColor]::DarkGreen }
+    elseif ($level -in @('skip')) { [Console]::ForegroundColor = [ConsoleColor]::Yellow }
+    elseif ($level -in @('warn', 'warning')) { [Console]::ForegroundColor = [ConsoleColor]::DarkYellow }
+    elseif ($level -in @('fail', 'failure', 'error')) { [Console]::ForegroundColor = [ConsoleColor]::Red }
+    elseif ($level -eq 'fatal') { [Console]::ForegroundColor = [ConsoleColor]:: DarkRed }
+    [Console]::Error.WriteLine($logMessage)
+  } finally { [Console]::ResetColor() }
+  if ($LogFile) {
+    try {
+      Add-Content -Path $LogFile -Value $logMessage -Force -ErrorAction Stop
+    } catch {
+      $LogFile = $null
+      throw "Failed to write to log file '$LogFile': $($_.Exception.Message)"
+    }
+  }
+}
+
 function Out-Colored {
   # .SYNOPSIS
   # 	Outputs colored text to the output stream.
@@ -60,7 +84,7 @@ function Out-ColoredStderr {
   if ($local:output = Out-Colored @args) { [Console]::Error.WriteLine($local:output)	}
 }
 
-function Out-Help {
+function _yana_usage {
   # .SYNOPSIS
   # 	Outputs help information for the specified mode.
   #   If mode is not specified, displays general help information.
@@ -70,56 +94,44 @@ function Out-Help {
   )
   switch ($Mode) {
     'apply' {
-      Write-Host @'
-Usage: yana.ps1 apply <options>
-  Applies the specified YANA Module.
-
-Options:
-  -source <path|url>         Specifies the source of YANA Module to apply.
-'@
+      Write-Host 'Usage: yana.ps1 apply -source <path|url> [-routine <name>]'
+      Write-Host '  Applies the specified YANA Module.'
+      Write-Host 'Options:'
+      Write-Host '  -source <path|url>         Specifies the source of YANA Module to apply. Can be a local path or a URL. Uses YANA_SOURCE environment variable.'
+      Write-Host '  -routine <name>            Specifies the routine to execute within the YANA Module. Uses YANA_ROUTINE environment variable.'
       break
     }
     'verify' {
-      Write-Host @'
-Usage: yana.ps1 verify <options>
-  Compares the state of the system with the state specified by the YANA Module without making any changes.
-
-Options:
-  -source <path|url>         Specifies the source of YANA Module to verify.
-'@
+      Write-Host 'Usage: yana.ps1 verify -source <path|url> [-routine <name>]'
+      Write-Host '  Compares the state of the system with the state specified by the YANA Module without making any changes.'
+      Write-Host 'Options:'
+      Write-Host '  -source <path|url>         Specifies the source of YANA Module to verify. Can be a local path or a URL. Uses YANA_SOURCE environment variable.'
+      Write-Host '  -routine <name>            Specifies the routine to execute within the YANA Module. Uses YANA_ROUTINE environment variable.'
       break
     }
     'fetch' {
-      Write-Host @'
-Usage: yana.ps1 fetch <options>
-  Fetches the specified YANA Module.
-
-Options:
-  -source <path|url>         Specifies the source of YANA Module to fetch.
-'@
-      break
+      Write-Host 'Usage: yana.ps1 fetch -source <path|url>'
+      Write-Host '  Fetches the specified YANA Module from the given source (path or URL).'
+      Write-Host 'Options:'
+      Write-Host '  -source <path|url>         Specifies the source of YANA Module to fetch. Can be path or URL. Uses YANA_SOURCE environment variable.'
+    }
+    'version' {
+      Write-Host 'Usage: yana.ps1 version'
+      Write-Host '  Displays the version of YANA.'
     }
     default {
-      Write-Host @'
-Usage: yana.ps1 <general options> [mode] <mode options>
-
-Modes:
-  apply                      Applies the specified YANA Module.
-  verify                     Compares the state of the system with the state specified by the YANA Module without making any changes.
-  fetch                      Fetches the specified YANA Module.
-'@
+      Write-Host 'Usage: yana.ps1 <general options> [mode] <mode options>'
+      Write-Host 'Modes:'
+      Write-Host '  version                    Displays the version of YANA.'
+      Write-Host '  apply                      Applies the specified YANA Module.'
+      Write-Host '  verify                     Compares the state of the system with the state specified by the YANA Module without making any changes.'
+      Write-Host '  fetch                      Fetches the specified YANA Module.'
     }
   }
-  Write-Host @'
-
-General Options:
-  -version                   Displays the version of YANA.
-  -help                      Displays this help message.
-  -help <mode>               Displays help for the specified mode.
-  -logfile <path>            Log file path. Uses YANA_LOGFILE environment variable. If not specified, logs are not written to a file.
-  -quiet                     Suppresses output messages. Uses YANA_QUIET environment variable if set.
-  -nocolor                   Disables colored output. Uses YANA_NOCOLOR environment variable if set.
-'@
+  Write-Host 'General Options:'
+  Write-Host '  -help                      Displays this help message.'
+  Write-Host '  -help <mode>               Displays help for the specified mode.'
+  Write-Host '  -logfile <file>            Log file path. Uses YANA_LOGFILE environment variable. If not specified, logs are not written to a file.'
 
 }
 
@@ -159,48 +171,41 @@ function Invoke-YanaFetch {
   # Placeholder for actual implementation of fetching the YANA module
 }
 
-function Invoke-Yana {
+function _yana_ {
   # .SYNOPSIS
   # 	The main entry point for YANA.
   param(
-    # If specified, outputs the version and exits.
-    [switch]$Version,
     # If specified, outputs help information and exits.
     [switch]$Help,
     [Parameter(Position = 0)]
+    [ValidateSet('apply', 'verify', 'fetch', 'version')]
     [string]$Mode = $Env:YANA_MODE,
+    # If specified, the source of the YANA Module to apply/verify/fetch.
     [Parameter(Position = 1)]
     [string]$Source = $Env:YANA_SOURCE,
+    # If specified, the routine of the YANA Module to apply/verify/fetch.
+    [Parameter(Position = 2)]
+    [string]$Routine = $Env:YANA_ROUTINE,
     # If specified, outputs log messages to the given file.
     # Uses YANA_LOGFILE environment variable if set.
     [string]$LogFile = $Env:YANA_LOGFILE,
-    # If specified, suppresses output messages.
-    # Uses YANA_QUIET environment variable if set.
-    [switch]$Quiet = "$Env:YANA_QUIET" -notin ('0', 'false', ''),
-    # If specified, disables colored output.
-    # Uses YANA_NOCOLOR environment variable if set.
-    [switch]$NoColor = "$Env:YANA_NOCOLOR" -notin ('0', 'false', '')
+    # If specified, enables debug level logging.
+    [switch]${-Debug} = $Env:YANA_DEBUG -eq 'true',
+    # If specified, enables trace level logging.
+    [switch]${-Trace} = $Env:YANA_TRACE -eq 'true'
   )
   # Disable progress bar output
   $Script:ProgressPreference = 'SilentlyContinue'
+  log info "$Script:YANA_TITLE Version: $Script:YANA_VERSION"
+  $Script:YANA_TRACE = [bool]${-Trace}
+  $Script:YANA_DEBUG = [bool]${-Debug} -or $Script:YANA_TRACE
 
-  Out-ColoredStderr -Message $Script:YANA_TITLE -MessageDetail "Version: $Script:YANA_VERSION"
-
-  if ($Help) { Out-Help -Mode $Mode; return }
-  if ($Version) { Write-Output $Script:YANA_VERSION; return }
+  if ($Help) { _yana_usage -Mode $Mode; return }
   switch ($Mode) {
-    'apply' {
-      Invoke-YanaApply -Source $Source
-      break
-    }
-    'verify' {
-      Invoke-YanaVerify -Source $Source
-      break
-    }
-    'fetch' {
-      Invoke-YanaFetch -Source $Source
-      break
-    }
+    'apply' { Invoke-YanaApply -Source $Source }
+    'verify' { Invoke-YanaVerify -Source $Source }
+    'fetch' { Invoke-YanaFetch -Source $Source }
+    'version' { $Script:YANA_VERSION }
     '' {
       throw 'No mode specified. Use -help to see available modes.'
     }
@@ -213,12 +218,10 @@ function Invoke-Yana {
 # Prevent running when dot-sourced
 if ($MyInvocation.InvocationName -ne '.') {
   try {
-    Invoke-Yana @args
+    _yana_ @args
   } catch {
-    $_fc = [Console]::ForegroundColor
-    [Console]::ForegroundColor = 'Red'
-    [Console]::Error.WriteLine("Error: $($_.Exception.Message)")
-    [Console]::ForegroundColor = $_fc
+    log fatal "An unexpected error occurred at line $($_.InvocationInfo.ScriptLineNumber): $($_.Exception.Message)"
+    $_.ScriptStackTrace | ForEach-Object { log trace $_ }
     exit 1
   }
 }
